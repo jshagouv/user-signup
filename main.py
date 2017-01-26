174ffe7eb14a9c9f493ff5f812253851c91a27b5
Jill Shaddy-Gouvion <jshaddy75@gmail.com>
2017-01-25 19:26:50 -0600

    Create basic form with POST action to same page

--- main.py
+++ main.py
@@ -14,11 +14,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# QUESTIONS:
+# Can form action be changed? If form isn't valid, we want to stay on page, if
+#       it is valid, we want to go to another page.
 import webapp2
 
+page_header = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Signup Form</title>
+    </head>
+    <body>
+"""
+
+page_footer = """
+    </body>
+</html>
+"""
+
+signup_form = """
+<h1>Sign Up!</h1>
+<form method = "post" action="/" name="signup-form">
+    <label>Username
+        <input name="username" value="%(user_name)s"/>
+    </label>
+    <br>
+    <label>Password
+        <input name="password"/>
+    </label>
+    <br>
+    <label>Verify Password
+        <input name="verify"/>
+    </label>
+    <br>
+    <label>Email (optional)
+        <input name="email" value="%(email_addy)s"/>
+    </label>
+    <br>
+    <button type="submit">Submit</button>
+</form>
+"""
+content = page_header + signup_form + page_footer
+
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, user_name="", email_addy=""):
+        self.response.out.write(content % {"user_name":user_name,
+                                           "email_addy":email_addy})
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+    def post(self):
+        user_name = self.request.get("username")
+        email_addy = self.request.get("email")
+        self.write_form(user_name,email_addy)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
