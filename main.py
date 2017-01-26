639870cfa14ba9a4c9c76b1a273a8facdf2a09f2
Jill Shaddy-Gouvion <jshaddy75@gmail.com>
2017-01-25 22:08:10 -0600

    Add validity check
    Added validation checks for all form entries. Invalid entries will be
    noted with an error message. If all entries valid, will redirect to
    welcome page.

--- main.py
+++ main.py
@@ -18,7 +18,11 @@
 # QUESTIONS:
 # Can form action be changed? If form isn't valid, we want to stay on page, if
 #       it is valid, we want to go to another page.
+# Can python variable names match html element names?
+# Why didn't /w work in character class for re.compile for username?
+
 import webapp2
+import re
 
 page_header = """
 <!DOCTYPE html>
@@ -38,19 +42,19 @@ signup_form = """
 <h1>Sign Up!</h1>
 <form method = "post" action="/" name="signup-form">
     <label>Username
-        <input name="username" value="%(user_name)s"/>
+        <input name="username" value="%(user_name)s"/> &#8198; <label>%(username_msg)s</label>
     </label>
     <br>
     <label>Password
-        <input name="password"/>
+        <input name="password"/> &#8198; <label>%(password_msg)s</label>
     </label>
     <br>
     <label>Verify Password
-        <input name="verify"/>
+        <input name="verify"/> &#8198; <label>%(verify_msg)s</label>
     </label>
     <br>
     <label>Email (optional)
-        <input name="email" value="%(email_addy)s"/>
+        <input name="email" value="%(email_addy)s"/> &#8198; <label>%(email_msg)s</label>
     </label>
     <br>
     <button type="submit">Submit</button>
@@ -58,19 +62,73 @@ signup_form = """
 """
 content = page_header + signup_form + page_footer
 
+
+# re (regex) module usage notes: to avoid issues with both
+# python and regex module using \ to escape special
+# characters, use python's raw string notation. Prefix string
+# literal with 'r'
+# $ signifies end of string before a newline
+def is_valid_username(username):
+    # \w should match any alphanumeric character == [a-zA-Z0-9_]
+    # - matches a literal - (in Udacity notes, provided
+    # r"^[a-zA-Z0-9_-]{3,20}$", I changed it to [/w-] after
+    # reading re HOWTO). /w didn't work, went back to Udacity's
+    USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+    return USER_RE.match(username)
+def is_valid_password(password):
+    # . matches anything except a newline character
+    PASSWORD_RE = re.compile(r"^.{3,20}$")
+    return PASSWORD_RE.match(password)
+def is_valid_email(email):
+    # \S matches any non-whitespace character
+    # . is literal in this case (I think because of the 'r' prefix)
+    EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+    return EMAIL_RE.match(email)
+
+def validity_check(username, password, verify, email):
+    #error_msg = "<style color=red><b>%s</b></style>"
+    error_flag = False
+    username_msg = password_msg = verify_msg = email_msg = ""
+    if not is_valid_username(username):
+        username_msg = "Please provide a valid username"
+        error_flag = True
+    if not is_valid_password(password):
+        password_msg = "Please provide a valid password"
+        error_flag = True
+    if password != verify:
+        verify_msg = "Verify Password does not match Password"
+        error_flag = True
+    if len(email) > 0:
+        if not is_valid_email(email):
+            email_msg = "Please provide a valid email"
+            error_flag = True
+    if error_flag:
+        return (username_msg, password_msg, verify_msg, email_msg)
+
 class MainHandler(webapp2.RequestHandler):
-    def write_form(self, user_name="", email_addy=""):
+    def write_form(self, user_name="", email_addy="", username_msg="",
+                    password_msg="", verify_msg="", email_msg=""):
         self.response.out.write(content % {"user_name":user_name,
-                                           "email_addy":email_addy})
+                                           "email_addy":email_addy,
+                                           "username_msg":username_msg,
+                                           "password_msg":password_msg,
+                                           "verify_msg":verify_msg,
+                                           "email_msg":email_msg})
     def get(self):
         self.write_form()
     def post(self):
         user_name = self.request.get("username")
+        pass_word = self.request.get("password")
+        verify_password = self.request.get("verify")
         email_addy = self.request.get("email")
-        if user_name:
+        invalid_result = validity_check(user_name,pass_word,verify_password,
+                                        email_addy)
+        if not invalid_result:
             self.redirect("/welcome?username=" + user_name)
         else:
-            self.write_form(user_name,email_addy)
+            self.write_form(user_name,email_addy,invalid_result[0],
+                            invalid_result[1],invalid_result[2],
+                            invalid_result[3])
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
